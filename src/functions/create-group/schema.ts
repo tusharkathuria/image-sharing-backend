b383b568791da2266cd5f2987b02ecd34dbaa484
Tushar Kathuria <tushar.kathuria@compass.com>
2021-04-11 17:10:46 +0530

    Removed redundant files

--- src/functions/create-group/schema.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    id: {type: 'string'},
-    name: { type: 'string' },
-    description: {type: 'string'}
-  },
-  required: ['id', 'name']
-} as const;
--- src/functions/create-image/schema.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    title: {title: 'string'}
-  },
-  required: ["title"]
-} as const;
